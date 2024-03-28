aec5724125ccde970e48deb14190ccc77324b939
Fabio Kulka <fabiokulka.67@gmail.com>
2024-03-28 14:23:04 -0300

    Teste endpoint

---
+++ src/main/java/com/github/restwithspringboot/RestwithspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.github.restwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwithspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/restwithspringboot/RestwithspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwithspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
