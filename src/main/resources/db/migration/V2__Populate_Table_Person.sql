f253768bf9f8f864accce7700b5b831d68baaddb
Fabio Kulka <fabiokulka.67@gmail.com>
2024-03-31 20:57:06 -0300

    Migrations com flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `tb_person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Curitiba', 'Fernando', 'Male', 'Kulka'),
+	(2, 'Curitiba', 'Fernando', 'Male', 'Kulka'),
+	(3, 'Curitiba', 'Fernando', 'Male', 'Kulka'),
+	(4, 'Campo Largo', 'Matheus', 'Male', 'Kulka');
