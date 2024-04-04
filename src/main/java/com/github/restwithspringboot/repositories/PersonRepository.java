f2c7e062c52c139e3d70fc2be793a5f7f9a4a532
Fabio Kulka <fabiokulka.67@gmail.com>
2024-04-04 19:15:10 -0300

    Ajustando a base de dados

--- src/main/java/com/github/restwithspringboot/repositories/PersonRepository.java
+++ src/main/java/com/github/restwithspringboot/repositories/PersonRepository.java
@@ -3,4 +3,6 @@ package com.github.restwithspringboot.repositories;
 
 import com.github.restwithspringboot.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+//repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
