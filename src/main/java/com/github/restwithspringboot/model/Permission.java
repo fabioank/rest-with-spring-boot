db3cf723669238b5cd79b13b91f801bb873ce2f4
Fabio Kulka <fabiokulka.67@gmail.com>
2024-04-04 19:49:44 -0300

    Implementando o spring security

---
+++ src/main/java/com/github/restwithspringboot/model/Permission.java
@@ -0,0 +1,48 @@
+package com.github.restwithspringboot.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String description;
+
+    public Permission (){}
+
+    public Permission(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+}
